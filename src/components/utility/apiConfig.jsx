342658ff9846d0cb973de4ba7187df0ffbdeff54
Joel Davenport <joelmike46@gmail.com>
2023-03-31 18:39:27 -0400

    add log so I can see what's happening

--- src/components/utility/apiConfig.jsx
+++ src/components/utility/apiConfig.jsx
@@ -3,12 +3,14 @@ const apiVersion = 'v1';
 
 const env = process.env.NODE_ENV
 
+console.log(process.env)
+
 if (env === 'development') {
     backendHost = "http://localhost:8000";
 } else if (env === 'gcloud_development') {
     backendHost = 'https://dev-api.glyphassistant.com';
 } else {
-    backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
+    backendHost = process.env.BACKEND_HOST || 'http://localhost:8080';
 }
 
 export const API_ROOT = `${backendHost}`;
@@ -18,7 +20,7 @@ if (env === 'development') {
 } else if (env === 'gcloud_development') {
     backendHost = 'wss://dev-api.glyphassistant.com';
 } else {
-    backendHost = process.env.REACT_APP_BACKEND_HOST || 'ws://localhost:8080';
+    backendHost = process.env.BACKEND_HOST || 'ws://localhost:8080';
 }
 
 export const WS_ROOT = `${backendHost}`;
