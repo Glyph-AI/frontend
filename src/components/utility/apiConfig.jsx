e83421dc2c59f9918c424cdcd1b8359009dabf4c
Joel Davenport <joelmike46@gmail.com>
2023-03-31 19:37:00 -0400

    set host

--- src/components/utility/apiConfig.jsx
+++ src/components/utility/apiConfig.jsx
@@ -1,26 +1,3 @@
-let backendHost;
-const apiVersion = 'v1';
+export const API_ROOT = `https://dev-api.glyphassistant.com`;
 
-const env = process.env.NODE_ENV
-
-console.log(process.env)
-
-if (env === 'development') {
-    backendHost = "http://localhost:8000";
-} else if (env === 'gcloud_development') {
-    backendHost = 'https://dev-api.glyphassistant.com';
-} else {
-    backendHost = process.env.BACKEND_HOST || 'http://localhost:8080';
-}
-
-export const API_ROOT = `${backendHost}`;
-
-if (env === 'development') {
-    backendHost = "ws://localhost:8000";
-} else if (env === 'gcloud_development') {
-    backendHost = 'wss://dev-api.glyphassistant.com';
-} else {
-    backendHost = process.env.BACKEND_HOST || 'ws://localhost:8080';
-}
-
-export const WS_ROOT = `${backendHost}`;
+export const WS_ROOT = `wss://dev-api.glyphassistant.com`;
