6d33db8a73f722d946585d3bf1d6aefa5c90706d
Joel Davenport <joelmike46@gmail.com>
2023-03-31 18:11:43 -0400

    update api config and dockerfile for development

--- src/components/utility/apiConfig.jsx
+++ src/components/utility/apiConfig.jsx
@@ -6,7 +6,7 @@ const env = process.env.NODE_ENV
 if (env === 'development') {
     backendHost = "http://localhost:8000";
 } else if (env === 'gcloud_development') {
-    backendHost = 'https://dev.api.glyphassistant.com';
+    backendHost = 'https://dev-api.glyphassistant.com';
 } else {
     backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
 }
