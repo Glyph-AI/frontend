cca352383dfb396a72a739202e2e20ed860d8bcd
Joel Davenport <joelmike46@gmail.com>
2023-03-31 13:22:00 -0400

    disable eslint in prod builds

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, context) => {
     // Enable polling based on env variable being set
     if (process.env.NEXT_WEBPACK_USEPOLLING) {
