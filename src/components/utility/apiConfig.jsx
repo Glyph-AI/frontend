f8ea78bddc85253480e24f0a1588249a5216e843
Joel Davenport <joelmike46@gmail.com>
2023-04-21 12:28:38 -0400

    fix another issue

--- src/components/utility/apiConfig.jsx
+++ src/components/utility/apiConfig.jsx
@@ -5,8 +5,6 @@ if (env === 'local') {
     backendHost = "http://localhost:8000";
 } else if (env === 'development') {
     backendHost = 'https://dev-api.glyphassistant.com';
-} else if (/^qa/.test(hostname)) {
-    backendHost = `https://api.${hostname}`;
 } else {
     backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
 }
--- src/components/utility/gLogin.jsx
+++ src/components/utility/gLogin.jsx
@@ -5,8 +5,6 @@ if (env === 'local') {
     gLogin = "991199193983-h5pq059ivvtim8q0hp86d7asigjbbjcc.apps.googleusercontent.com";
 } else if (env === 'development') {
     gLogin = '520170255321-a0i4dvb074idd0q7dcrh4g0mb5gqvaut.apps.googleusercontent.com';
-} else if (/^qa/.test(hostname)) {
-    gLogin = "dummy";
 } else {
     gLogin = "dummy";
 }
