226158448297b4e6a77b0a4e21a1748b63f91d10
Joel Davenport <joelmike46@gmail.com>
2023-04-23 17:12:13 -0400

    add avatar

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -4,6 +4,19 @@ import {
     ConversationHeader
 } from '@chatscope/chat-ui-kit-react'
 import { useRouter } from "next/router";
+import { Avatar, Badge, Box } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import { theme } from "@/components/utility/theme";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+    width: 32,
+    height: 32,
+    color: "rgba(255, 255, 255, 1)",
+    '&:hover': {
+        border: "2px solid #fff"
+    }
+}));
 
 export default function Profile() {
     const router = useRouter()
@@ -27,6 +40,28 @@ export default function Profile() {
                     <ConversationHeader.Back onClick={() => { router.push("/conversations") }} />
                     <ConversationHeader.Content userName="Profile" />
                 </ConversationHeader>
+                <Box sx={{ display: "flex", alignContent: "center", justifyContent: "center", padding: "8px" }}>
+                    <Badge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        onClick={() => { setProfileDialogOpen(true) }}
+                        badgeContent={
+                            <SmallAvatar>
+                                <FileUploadIcon />
+                            </SmallAvatar>
+                        }
+                    >
+                        <Avatar
+                            // src={
+                            //     email && `http://localhost:8000/users/${email}/profile-picture`
+                            // }
+                            sx={{ height: 128, width: 128, fontSize: 90, backgroundColor: "#fff" }}
+                            alt={"Avatar"}
+                        >
+
+                        </Avatar>
+                    </Badge>
+                </Box>
 
             </motion.div>
         </Layout>
