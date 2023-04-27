c340a54ec667cef5f59263b0a20e8daf411badfd
Joel Davenport <joelmike46@gmail.com>
2023-04-27 10:09:12 -0400

    for gods sake it works

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,17 @@ import '@/styles/globals.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import '@/styles/chat.css'
 import { AnimatePresence } from 'framer-motion'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    window.addEventListener("resize", (ev) => {
+      document.body.style.height = window.visualViewport.height + "px"
+      console.log(document.body.style)
+    }, true)
+  }, [])
+
   return (
     <AnimatePresence mode="wait" initial={false}>
       <Component {...pageProps} />
