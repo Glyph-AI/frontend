953f64b58412b86147b8718f3c320993c8250220
Joel Davenport <joelmike46@gmail.com>
2023-05-02 10:25:35 -0400

    update configs to add production values

--- src/components/utility/gLogin.jsx
+++ src/components/utility/gLogin.jsx
@@ -5,6 +5,8 @@ if (env === 'local') {
     gLogin = "991199193983-h5pq059ivvtim8q0hp86d7asigjbbjcc.apps.googleusercontent.com";
 } else if (env === 'development') {
     gLogin = '520170255321-a0i4dvb074idd0q7dcrh4g0mb5gqvaut.apps.googleusercontent.com';
+} else if (env === 'production') {
+    gLogin = '511935008815-hu6p9h33o601ht85gq13b7s1ed89das4.apps.googleusercontent.com';
 } else {
     gLogin = "991199193983-h5pq059ivvtim8q0hp86d7asigjbbjcc.apps.googleusercontent.com";
 }
