f276d155f6e6e0edfad45b864f342bc11b821847
Joel Davenport <joel.davenport@ntient.ai>
2023-05-04 21:17:07 -0400

    add normal sign-up flow

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,10 +4,19 @@ import { useRouter } from 'next/router';
 import { GOOGLE_LOGIN_KEY } from '@/components/utility/gLogin';
 import Layout from '@/components/utility/layout';
 import { motion } from "framer-motion";
+import { Box } from '@mui/system';
+import { Button, Divider, TextField } from '@mui/material';
 
 export default function Login() {
     const [redirectUrl, setRedirectUrl] = useState("/conversations")
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ passwordConfirm, setPasswordConfirm ] = useState("")
+    const [ firstName, setFirstName ] = useState("")
+    const [ lastName, setLastName ] = useState("")
+    const [ displaySignup, setDisplaySignup ] = useState(false)
     const router = useRouter()
+    
     const handleGoogle = (resp) => {
         var auth_data = {
             "token": resp.credential
@@ -18,6 +27,41 @@ export default function Login() {
                 router.push(redirectUrl)
             } else if (status === 401) {
                 console.log("Failed")
+            }
+        }, { "Content-Type": "application/json" })
+    }
+
+    const handleSignin = () => {
+        const data = {
+            "email": email,
+            "password": password
+        }
+
+        genericRequest("/login", "POST", JSON.stringify(data), (data, status) => {
+            if (status === 200) {
+                router.push(redirectUrl)
+            } else if (status === 401) {
+                // raise some kind of thing here
+                console.log("failed")
+            }
+        }, { "Content-Type": "application/json" })
+    }
+
+    const handleSignup = () => {
+        const data = {
+            "email": email,
+            "password": password,
+            "first_name": firstName,
+            "last_name": lastName,
+            "role": "user"
+        }
+
+        genericRequest("/users", "POST", JSON.stringify(data), (data, status) => {
+            if (status === 200) {
+                router.push(redirectUrl)
+            } else if (status === 401) {
+                // raise some kind of thing here
+                console.log("failed")
             }
         }, { "Content-Type": "application/json" })
     }
@@ -47,17 +91,69 @@ export default function Login() {
             setRedirectUrl(`/bots?bot_code=${bot_code}`)
         }
     }, [handleGoogle])
+
+    const signUpDisabled = () => {
+        if ( firstName !== "" &&
+            lastName !== "" &&
+            email != "" &&
+            password !== "" &&
+            passwordConfirm !== "" &&
+            password === passwordConfirm
+        ) {
+            return false
+        }
+
+        return true
+    }
+
+    const renderLogin = () => {
+        return (
+            <>
+                <Box sx={{ width: "100%", flexWrap: "wrap", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <TextField value={email} onChange={(ev) => {setEmail(ev.target.value)}} label="E-Mail" sx={{width: "90%"}}></TextField>
+                    <TextField value={password} onChange={(ev) => {setPassword(ev.target.value)}} label="Password" type="password" sx={{width: "90%", marginTop: "8px"}}></TextField>
+                </Box>
+                <Box sx={{width: "100%", flexWrap: "wrap", display: "flex", alignItems: "center", justifyContent: "Center"}}>
+                    <Button onClick={() => {handleSignin()}} variant="contained" sx={{marginTop: "16px", width: "60%"}}>Sign-In</Button>
+                    <Button variant="outlined" sx={{marginTop: "16px", width: "60%"}} onClick={() => {setDisplaySignup(true)}}>Sign-Up</Button>
+                </Box>
+            </>
+        )
+    }
+
+    const renderSignup = () => {
+        return (
+            <>
+                <Box sx={{ width: "100%", flexWrap: "wrap", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <TextField value={email} onChange={(ev) => {setEmail(ev.target.value)}} label="E-Mail" sx={{width: "90%"}}></TextField>
+                    <TextField value={firstName} onChange={(ev) => {setFirstName(ev.target.value)}} label="First Name" sx={{width: "90%", marginTop: "8px"}}></TextField>
+                    <TextField value={lastName} onChange={(ev) => {setLastName(ev.target.value)}} label="Last Name" sx={{width: "90%", marginTop: "8px"}}></TextField>
+                    <TextField value={password} onChange={(ev) => {setPassword(ev.target.value)}} label="Password" type="password" sx={{width: "90%", marginTop: "8px"}}></TextField>
+                    <TextField value={passwordConfirm} onChange={(ev) => {setPasswordConfirm(ev.target.value)}} label="Confirm Password" type="password" sx={{width: "90%", marginTop: "8px"}}></TextField>
+                </Box>
+                <Box sx={{width: "100%", flexWrap: "wrap", display: "flex", alignItems: "center", justifyContent: "Center"}}>
+                    <Button disabled={signUpDisabled()} onClick={() => {handleSignup()}} variant="contained" sx={{marginTop: "16px", width: "60%"}}>Sign-Up</Button>
+                    <Button onClick={() => {setDisplaySignup(false)}} variant="outlined" sx={{marginTop: "16px", width: "60%"}}>Back to Sign-in</Button>
+                </Box>
+            </>
+        )
+    }
+
+
     return (
         <Layout>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-                <div style={{ width: "50%" }}>
-                    <div>
-                        <img className="logo" alt="Glyph Logo" src={"/dark_vertical.png"} style={{ width: "100%", marginBottom: "8px" }} />
-                        {/* <div style={{ textAlign: "center", fontSize: 40, marginBottom: "8px" }}>GlyphAI</div> */}
-                        <div style={{ width: "100%", display: "flex", justifyContent: "center" }} id="gLogin" />
-                    </div>
-                </div>
-            </div>
+            <Box style={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <Box style={{ width: "100%" }}>
+                    <Box sx={{alignContent: "center", justifyContent: "center"}}>
+                        <Box sx={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                            <img className="logo" alt="Glyph Logo" src={"/dark_vertical.png"} style={{ width: "50%", marginBottom: "8px" }} />
+                        </Box>
+                        {displaySignup ? renderSignup() : renderLogin()}
+                        <Divider sx={{marginTop: "16px"}} variant="middle" >OR</Divider>
+                        <div style={{ marginTop: "16px", width: "100%", display: "flex", justifyContent: "center" }} id="gLogin" />
+                    </Box>
+                </Box>
+            </Box>
         </Layout>
     )
 }
