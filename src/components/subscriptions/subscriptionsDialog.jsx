be8ccf2f84ca56f6777f95b952bf7f24dc29f8c0
Joel Davenport <joelmike46@gmail.com>
2023-09-22 12:33:00 -0400

    more fixes for mobile

--- src/components/subscriptions/subscriptionsDialog.jsx
+++ src/components/subscriptions/subscriptionsDialog.jsx
@@ -1,9 +1,31 @@
-import { Dialog, DialogContent } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { AppBar, Dialog, DialogContent, IconButton, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import SubscriptionContainer from "./subscriptionContainer";
 
 export default function SubscriptionsDialog({ open, handleClose }) {
+    const theme = useTheme()
+    const smallScreen = useMediaQuery(theme.breakpoints.down("md"))
     return (
-        <Dialog open={open} onClose={handleClose}>
+        <Dialog open={open} onClose={handleClose} fullScreen={smallScreen}>
+            {
+                smallScreen && (
+                    <AppBar sx={{ position: 'relative' }}>
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                onClick={handleClose}
+                                aria-label="close"
+                            >
+                                <Close />
+                            </IconButton>
+                            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                                Subscription
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                )
+            }
             <DialogContent>
                 <SubscriptionContainer />
             </DialogContent>
